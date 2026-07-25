6cd1775cbf97169d535f6c187612c54e0c03ba0a
elenaberegova <elenaberegova@proton.me>
2026-07-25 22:31:28 +0300

    Create Database object with measurement units
    Added a Database object containing various measurement units with their details.

---
+++ Database.kt
@@ -0,0 +1,79 @@
+object Database {
+
+    fun units() = listOf(
+
+        Measurement(
+
+            "Cubit",
+
+            "Ancient Egypt",
+
+            0.523,
+
+            "Length from elbow to fingertip"
+
+        ),
+
+        Measurement(
+
+            "League",
+
+            "Europe",
+
+            4828.03,
+
+            "Long-distance travel unit"
+
+        ),
+
+        Measurement(
+
+            "Versta",
+
+            "Russia",
+
+            1066.8,
+
+            "Traditional Russian distance"
+
+        ),
+
+        Measurement(
+
+            "Arshin",
+
+            "Russia",
+
+            0.711,
+
+            "Common trade measurement"
+
+        ),
+
+        Measurement(
+
+            "Vershok",
+
+            "Russia",
+
+            0.04445,
+
+            "Small Russian length unit"
+
+        ),
+
+        Measurement(
+
+            "Furlong",
+
+            "England",
+
+            201.168,
+
+            "Agricultural distance"
+
+        )
+
+    )
+
+}
